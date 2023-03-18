f2f3275acd00bf184ae74f7d01c4e39ac6d72b41
daniel-suissa <suissa.daniel@gmail.com>
2023-03-18 13:35:51 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+const authorsName = "Jon Doe"
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is {authorsName}'s app!'.
         </p>
         <a
           className="App-link"
