c27bd55cf88b02c9217da43fbb7c6b87a565a811
daniel-suissa <suissa.daniel@gmail.com>
2023-03-18 20:11:00 +0200

    populate a table on a button click

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import Stack from '@mui/material/Stack';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import React, { useState } from 'react';
-
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import getNewRows from './getNewRows'
 import './App.css';
 
 function App() {
 
   const [textFieldInput, setTextFieldInput] = useState("")
+  const [rows, setRows] = useState([])
 
   const onTextFieldChange = (e) => {
     setTextFieldInput(e.target.value)
@@ -16,6 +24,7 @@ function App() {
 
   const onScanButtonClick = () => {
     console.log(textFieldInput)
+    setRows(getNewRows(textFieldInput))
   }
 
   return (
@@ -30,6 +39,29 @@ function App() {
             variant="filled"
             color='primary'/>
           <Button onClick={onScanButtonClick} sx={{maxWidth: "xs"}} variant="contained"> Scan Now </Button>
+          <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Bucket Name</TableCell>
+            <TableCell align="right">File Name</TableCell>
+            <TableCell align="right">Sensitive Data</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell align="right">{row.bucketName}</TableCell>
+              <TableCell align="right">{row.fileName}</TableCell>
+              <TableCell align="right">{row.sensitiveData}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
         </Stack>
       </Container>
     </div>
