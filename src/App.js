0c0a6ee76533d49c451102965f491f61fca26c24
daniel-suissa <suissa.daniel@gmail.com>
2023-03-18 13:39:29 +0200

    remove template ui code

--- src/App.js
+++ src/App.js
@@ -5,20 +5,6 @@ const authorsName = "Jon Doe"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is {authorsName}'s app!'.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
