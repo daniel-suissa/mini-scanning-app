26a673256ba2d71f9ca68f548460f9df5b2b07df
daniel-suissa <suissa.daniel@gmail.com>
2023-03-18 14:36:45 +0200

    add a button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Container>
-        <Stack sx={{marginTop: '20%'}} >
+        <Stack spacing={3} sx={{marginTop: '20%'}} >
           <TextField id="bukcket-name-input" label="Bucket Name" variant="filled" color='primary'/>
+          <Button sx={{maxWidth: "xs"}} variant="contained"> Scan Now </Button>
         </Stack>
       </Container>
     </div>
