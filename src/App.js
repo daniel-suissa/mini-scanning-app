ec4f846c79d033ef0ed239813ea19975e4fcdce3
daniel-suissa <suissa.daniel@gmail.com>
2023-03-18 18:22:08 +0200

    add useState to handle textfield changes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
+import React, { useState } from 'react';
 
 import './App.css';
 
 function App() {
+
+  const [textFieldInput, setTextFieldInput] = useState("")
+
+  const onTextFieldChange = (e) => {
+    setTextFieldInput(e.target.value)
+  }
+
   return (
     <div className="App">
       <Container>
         <Stack spacing={3} sx={{marginTop: '20%'}} >
-          <TextField id="bukcket-name-input" label="Bucket Name" variant="filled" color='primary'/>
+          <TextField
+            id="bukcket-name-input"
+            value={textFieldInput}
+            onChange={onTextFieldChange}
+            label="Bucket Name"
+            variant="filled"
+            color='primary'/>
           <Button sx={{maxWidth: "xs"}} variant="contained"> Scan Now </Button>
         </Stack>
       </Container>
